9181d85b402635c6a0e5fde55e9abc6952acbc22
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-09-30 20:22:32 -0300

    Adicionei o useNavigate ao final do cadastro do usuário

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 import { isEmail } from 'validator'
+import { useNavigate } from 'react-router-dom'
 
 import { Form } from './styled'
+import axios from '../../services/axios'
 
 export default function Register() {
+    const navigate = useNavigate()
+
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
 
         let formErrors = false
@@ -27,6 +31,22 @@ export default function Register() {
         if(password.length < 5 || password.length > 64) {
             formErrors = true
             toast.error('A SENHA deve ter entre 4 e 65 caracteres')
+        }
+
+        if(formErrors) return
+
+        try {
+            const user = await axios.post('/users', {
+                name: name,
+                email: email,
+                password: password
+            })
+
+            console.log(user)
+
+            navigate('/')
+        } catch(e) {
+            console.log('Houve um erro ' + e)
         }
     }
     return (
