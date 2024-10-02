a49dfdf4ae4cfee0e24af5f744ff6cbb9c187342
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-10-02 07:33:38 -0300

    Testei se a API está devolvendo todas as categorias e se a aplicação está conseguindo passar o token na requisição

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,7 +1,62 @@
+import { useState, useEffect } from 'react'
+
+import axios from '../../services/axios'
+import { Table } from './styled'
+
 export default function Categories() {
+    const [id, setId] = useState('')
+    //const [name, setName] = useState('')
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/categories',{
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+                }
+            })
+
+            console.log(response)
+
+            setId(response.data[0].id)
+        }
+
+        getData()
+    }, [])
+
     return (
         <>
-            <h1>Página de categorias</h1>
+            <h1>Categorias</h1>
+
+            <div>
+                <button>Nova Categoria</button>
+            </div>
+
+            <div>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>ATUALIZAR</th>
+                            <th>DELETAR</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{id}</td>
+                            <td>Exemplo 1</td>
+                            <td>Atualizar</td>
+                            <td>Deletar</td>
+                        </tr>
+                        <tr>
+                            <td>{id}</td>
+                            <td>Exemplo 2</td>
+                            <td>Atualizar</td>
+                            <td>Deletar</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
         </>
     )
 }
---
+++ src/pages/Categories/styled.js
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components'
+
+export const Table = styled.table`
+    margin-top: 30px;
+    border-collapse: collapse;
+    border: 1px solid black;
+
+    th, td {
+        padding: 8px;
+    }
+`
