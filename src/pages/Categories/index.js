8449a51382be853be452b8960813eb7554775663
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-10-02 22:25:44 -0300

    Finalizei o básico da rota de criar um nova categoria

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 import axios from '../../services/axios'
-import { Div, Table } from './styled'
+import { Container, Table } from './styled'
 
 export default function Categories() {
     const [categories, setCategories] = useState([])
+
+    const [newCategory, setNewCategory] = useState(false)
+    const [id, setId] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
         async function getData() {
@@ -18,13 +23,91 @@ export default function Categories() {
         getData()
     }, [])
 
+    function addCategory(e) {
+        e.preventDefault()
+        setNewCategory(true)
+    }
+
+    function cancelAddCategory(e) {
+        e.preventDefault()
+        setNewCategory(false)
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        let formErrors = false
+
+        if(!id) {
+            formErrors = true
+            toast.error('ID não informado')
+        }
+
+        if(id < 0) {
+            formErrors = true
+            toast.error('ID inválido')
+        }
+
+        if(!name) {
+            formErrors = true
+            toast.error('NOME não informado')
+        }
+
+        if(name.length < 3 || name.length > 30) {
+            formErrors = true
+            toast.error('O NOME deve ter entre 2 e 31 caracteres')
+        }
+
+        if(formErrors) return
+
+        try {
+            const response = await axios.post('/categories', {
+                id: Number(id),
+                name: name
+            })
+
+            console.log(response)
+            toast.success(`Categoria ${response.data.name} criada!`)
+        } catch(e) {
+            console.log('Houve um erro ' + e)
+
+
+            if(e.response) {
+                if(e.response.data.error) {
+                    e.response.data.error.map((err) => {
+                        return toast.error(err)
+                    })
+
+                    return
+                }
+                
+                toast.error('Houve um erro')
+            }
+        }
+    }
+
     return (
-        <Div>
+        <Container>
             <h1>Categorias</h1>
 
             <div>
-                <button className='createCategory'>Nova Categoria</button>
+                <button className='createCategory' onClick={addCategory}>Nova Categoria</button>
             </div>
+
+            {newCategory ? (
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor='id'>ID: </label>
+                        <input type='number' id='id' name='id' onChange={(e) => {setId(e.target.value)}} />
+
+                        <label htmlFor='name'>NOME: </label>
+                        <input type='text' id='name' name='name' onChange={(e) => {setName(e.target.value)}} />
+
+                        <button type='submit'>Criar</button>
+                        <button onClick={cancelAddCategory}>Cancelar</button>
+                    </form>
+                </div>
+            ) : null}
 
             <div>
                 <Table>
@@ -48,6 +131,6 @@ export default function Categories() {
                     </tbody>
                 </Table>
             </div>
-        </Div>
+        </Container>
     )
 }
--- src/pages/Categories/styled.js
+++ src/pages/Categories/styled.js
@@ -1,6 +1,6 @@
 import { styled } from 'styled-components'
 
-export const Div = styled.div`
+export const Container = styled.div`
     .createCategory {
         margin: 30px 0px;
     }
